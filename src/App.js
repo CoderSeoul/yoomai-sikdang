deec2b17647b1beef6d00a018fa58ce96797c9ee
CoderSeoul <realhustlecoder@gmail.com>
2021-01-15 19:54:25 +0900

    real first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
 import React from 'react';
-import {Route,Switch} from 'react-router-dom';
-import {routes} from 'lib/routes/routes';
-import {Helmet} from 'react-helmet';
+import { Route, Switch } from 'react-router-dom';
+import { routes } from 'lib/routes/routes';
+import { Helmet } from 'react-helmet';
 
 function App() {
 
   return (
     <>
-      <Helmet htmlAttributes={{lang:"en"}}
+      <Helmet htmlAttributes={{ lang: "kor" }}
         meta={[{
-          name:'des',
-          content:'content'
+          name: '우마이식당',
+          content: '라면'
         }]}
-        title="범계 우마이식당"/>
-        <Switch>
-          {
-            routes.map(({path,page,exact},i)=>(
-              <Route exact={exact} path={path} component={page} key={i}/>
-            ))
-          }
-      </Switch>     
+        title="범계 우마이식당" />
+      <Switch>
+        {
+          routes.map(({ path, page, exact }, i) => (
+            <Route exact={exact} path={path} component={page} key={i} />
+          ))
+        }
+      </Switch>
     </>
   );
 }
